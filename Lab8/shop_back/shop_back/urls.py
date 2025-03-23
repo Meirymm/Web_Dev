7d207cfc424241b5440661972c157ba82c77fe66
Meirymm <m_sultan@kbtu.kz>
2025-03-23 17:41:07 +0600

    finished

---
+++ Lab8/shop_back/shop_back/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to the home page!")
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+    path("", home),
+]
+
